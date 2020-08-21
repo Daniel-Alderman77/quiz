6a17981dde92f9fd26755212e560cabe14d4a861
Daniel Alderman <daniel.alderman77@gmail.com>
2020-08-21 14:18:48 +0100

    Iterates over each question asking them one after another and totalling up correct number answered

--- cmd/quiz-master/main.go
+++ cmd/quiz-master/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/csv"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 const file = "problems.csv"
@@ -18,10 +21,28 @@ func main() {
 
 	r := csv.NewReader(bytes.NewReader(b))
 
-	records, err := r.ReadAll()
+	questions, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print(records)
+	var correct int
+	for i, q := range questions {
+		fmt.Printf("Q%d, what is %v?\n", i+1, q[0])
+
+		reader := bufio.NewReader(os.Stdin)
+
+		answer, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		// convert CRLF to LF
+		answer = strings.Replace(answer, "\n", "", -1)
+
+		if answer == q[1] {
+			correct++
+		}
+	}
+
+	fmt.Printf("You got %d out of %d questions correct!", correct, len(questions))
 }
